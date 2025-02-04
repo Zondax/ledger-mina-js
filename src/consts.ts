ed27bfd8f5c07bb6a5f73bf56fe62a5b01852bbf
Andrés Benso <andresbenso@gmail.com>
2025-02-04 12:03:51 -0300

    use p1 to get or show address (#1)

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const enum P1_VALUES {
-  ONLY_RETRIEVE = 0x00,
-  SHOW_ADDRESS_IN_DEVICE = 0x01,
+  SHOW_ADDRESS_IN_DEVICE = 0x00,
+  ONLY_RETRIEVE = 0x01,
 }
 
 export const PUBKEYLEN = 55;
